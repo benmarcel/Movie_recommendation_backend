ee15d5c0abecfba22f05ebccecab01fbe03bca59
chima marcel <benmarcel381@gmail.com>
2025-06-23 14:59:17 +0100

    version 2.8

--- routes/user_auth.js
+++ routes/user_auth.js
@@ -2,8 +2,9 @@ import express from 'express';
 const router = express.Router();
 
 // import isAuthenticated from '../auth/isAuthenticated.js'; // Import your authentication middleware
-import checkSession from '../controllers/user_auth.js';
+import checkStatus from '../controllers/user_auth.js';
+import isAuthenticated from '../auth/isAuthenticated.js'; // Import your authentication middleware
 
 // Define the route for checking session status
-router.get('/me', checkSession); // Ensure the user is authenticated before accessing this route
+router.get('/me', isAuthenticated, checkStatus); // Ensure the user is authenticated before accessing this route
 export default router; // Export the router to be used in your main app file
